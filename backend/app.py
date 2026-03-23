1ee6662c2fd89fa722cc8b0accfd9fb6dbf8c8f2
Ksurekha21 <surekhakosimala@gmail.com>
2026-03-23 20:51:28 +0530

    cors fix

--- backend/app.py
+++ backend/app.py
@@ -15,10 +15,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 
-CORS(app, origins=[
-    "https://handloom-marketplace.vercel.app",
-    "https://handloom-marketplace-7awbans9c-ksurekha21s-projects.vercel.app"
-])
+CORS(app, resources={r"/*":{"origins":"*"}})
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\handloom-connect\\backend\\instance\\handloom_v7.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
