6a67c6bf983ee2723c01ac3fc09cd0d0f5fc514d
Ksurekha21 <surekhakosimala@gmail.com>
2026-03-24 10:54:38 +0530

    remove drop_all

--- backend/app.py
+++ backend/app.py
@@ -54,7 +54,7 @@ def home():
     return {"message": "Handloom Connect Backend Running"}
 
 with app.app_context():
-    db.drop_all()
+    
     db.create_all()
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
